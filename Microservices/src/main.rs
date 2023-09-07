5d857731daa079fe5a350ee9b5d71f4d14c82b78
cmndcntrlcyber <r.soreng@c3s.consulting>
2023-09-06 23:13:25 -0500

    Initial commit 	new file:   Lists/workers.txt 	new file:   Microservices/Cargo.toml 	new file:   Microservices/src/main.rs 	new file:   Rust Concurrency Reading

---
+++ Microservices/src/main.rs
@@ -0,0 +1,49 @@
+// This program is to define and spawn the microservices associated with Jawbreaker: 
+// A serverless C2 & payload delivery system. 
+// This is to be written to be deployed as a Cloudflare Worker, and initial access will spawn a child process from the victim browser to run the rest of the delivery-workers.
+// Dependencies
+
+// Land: Initial stager that has many functions calling to workers to add layers of encryption upon being called. No binary payloads exist within this file.
+fn land {
+    // GET requests to necessary AES-decrypt-workers
+    fn getkeys (
+    )
+    }
+
+    // Spread-Option 1 (spreadop1): Drop a LNK file to any open shares
+fn spreadop1 {
+    // Upon a service accessing the SMB share
+    fn camp (
+
+    ) 
+    // The LNK file will initiate impersonation and create a new shel as SYSTEM. 
+    // Based on SharpImpersonate 
+    fn impersonate (
+
+    ) 
+    // Perform an AD query to hosts & shares accessible by the user
+    // Based on Find-RemoteAdminAccess
+    fn seek {
+
+        // POST results to Store-Hosts worker
+
+    }
+
+    // Use SYSTEM shell and list of 
+    fn spread {
+
+    }
+    }
+// Link: This chunk of code is to establish the parameters for requests and delivery of payloads. Serves as the initial foothold
+
+// Encapsulated encrypted: The initial access payload calls a CF Worker to run a AES256 cypher against the chosen payload worker.  
+
+// Transport:
+
+
+
+
+fn main ()
+    [
+
+    ]
